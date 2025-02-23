70b13671e5d289d7e065d70bddfcb08cfb6a2cd6
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2025-02-23 07:32:00 +0700

    [Landing Page] : First Commit

---
+++ landing-page/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  window.location.href = 'https://cv.maqilm.site/'
+})
+</script>
+
+<template>
+  <p>Redirecting to CV...</p>
+</template>
